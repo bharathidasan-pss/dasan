fc62f578900323f1e87bb3bc9fe4ad55071bdff5
bharathidasan08 <101326013+bharathidasan08@users.noreply.github.com>
2023-01-22 10:15:38 +0530

    yes first test

---
+++ twoSum.js
@@ -0,0 +1,15 @@
+let  nums = [2,7,11,15], target = 9
+
+var twoSum = function(nums, target) {
+    for(let i=0;i<=nums.length-1;i++)
+{
+    for(let j=0;j<nums.length-1;j++){
+        if(i!=j){
+            if(nums[i]+nums[j]==target){
+                return [i,j]
+            }
+        }
+    }
+}
+};
+console.log(twoSum(nums,target));
